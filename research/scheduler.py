9304af6e7259095c8825ef094d58774973d493a2
tasddc1226 <tasddc@naver.com>
2022-05-11 16:30:28 +0900

    CHORE: logging 테스트를 위한 스케쥴 설정

--- research/scheduler.py
+++ research/scheduler.py
@@ -1,8 +1,9 @@
+from datetime import datetime
 from apscheduler.schedulers.background import BackgroundScheduler
 from .updater import batch_task
 
 def start():
     scheduler = BackgroundScheduler()
-    scheduler.add_job(batch_task, 'interval', seconds=5)
+    scheduler.add_job(batch_task, 'date', run_date=datetime.now())
     # scheduler.add_job(batch_task, 'interval', days=1)
     scheduler.start()
