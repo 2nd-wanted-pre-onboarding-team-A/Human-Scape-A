8f97c9dcaf650f7c4865038f0972a24b49f60a67
fore0919 <gms02294@gmail.com>
2022-05-11 14:46:58 +0900

    FIX:url수정

--- human/urls.py
+++ human/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
 
 urlpatterns = [
-    path('research', include('research.urls')),
+    path('/api/v1', include('research.urls')),
 ]
--- research/urls.py
+++ research/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from research.views import PublicDataListView
 
 urlpatterns = [
-    path('', PublicDataListView.as_view()),
+    path('research', PublicDataListView.as_view()),
     # path('/<int:number>', PublicDataDetailView.as_view())
 ]
