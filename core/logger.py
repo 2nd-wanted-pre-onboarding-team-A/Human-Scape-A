03cdf2caa9514073f675e42233d58beb42db446d
tasddc1226 <tasddc@naver.com>
2022-05-12 10:48:22 +0900

    FIX: pass_count 로깅 추가

--- core/logger.py
+++ core/logger.py
@@ -1,7 +1,7 @@
 import logging
 import logging.handlers
 
-def batch_task_logger(success_count, failure_count):
+def batch_task_logger(success_count, failure_count, pass_count):
     logger = logging.getLogger(__name__)
     formatter = logging.Formatter('[%(asctime)s][%(levelname)s] >> %(message)s')
     streamHandler = logging.StreamHandler()
@@ -12,4 +12,4 @@ def batch_task_logger(success_count, failure_count):
     logger.addHandler(streamHandler)
     logger.addHandler(fileHandler)
     logger.setLevel(level=logging.DEBUG)
-    logger.info(f'{success_count} rows created, {failure_count} rows failed.')
+    logger.info(f'{success_count} rows created, {failure_count} rows failed and {pass_count} rows passed')
